a60e9c0c557269b9f045cd822f042f411b8d9173
Karan Nijhawan <karannijhawan@gizmee.com>
2015-10-11 18:15:59 +0200

    tests

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 import test from "tape"
-// import fullscreener from "../src/"
+import fullscreener from "../src/"
 
 test("fullscreener", (t) => {
   console.log(t);
   t.plan(1)
-  t.equal(true, true, "return true")
+  t.equal(true, fullscreener(), "return true")
 })
